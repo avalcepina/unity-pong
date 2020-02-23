2d01fb2fbd38aa3a29b93ec4b8e61d0c235847e8
Alessandro Valcepina <valcepina@gmail.com>
2020-02-23 18:26:25 +0100

    Initial implementation

---
+++ Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{
+
+    public float moveSpeed = 15.0f;
+    public float movingBoundary = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetAxis("Horizontal") > 0f && transform.position.x < movingBoundary)
+            transform.Translate(new Vector3(0, 0, 1) * moveSpeed * Time.deltaTime);
+
+        if (Input.GetAxis("Horizontal") < 0f && transform.position.x > -movingBoundary)
+            transform.Translate(-new Vector3(0, 0, 1) * moveSpeed * Time.deltaTime);
+
+    }
+}
---
+++ Assets/Scripts/sphereController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sphereController : MonoBehaviour
+{
+    public float thrust = 2000.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.forward * -thrust);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
